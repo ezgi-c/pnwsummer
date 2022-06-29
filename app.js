1914a01aa8e53d57b932905f0635a78236411cc3
EZGI COBAN <SARAHEZGICOBAN@GMAIL.COM>
2022-06-28 23:07:36 -0700

    JS loops

--- app.js
+++ app.js
@@ -43,14 +43,79 @@ function dePixelate(){
      return document.write(image);
  }
 
-guess();
-function guess(){
-    let answer=prompt("Guess how many fingers I am holding");
-    if(answer=="3"){
-        alert("correct");
-        return;
-    } else {
-    alert("wrong answer");
-    }
-    guess();  
+// guess();
+// function guess(){
+//     let answer=prompt("Guess how many fingers I am holding");
+//     if(answer=="3"){
+//         alert("correct");
+//         return;
+//     } else {
+//     alert("wrong answer");
+//     }
+//     guess();  
+// }
+
+// function beachLoop(){
+//     let answer = prompt('How many secret beaches are there in Seattle?');
+//     while (answer != 149) {
+//         if (answer < 149) {
+//             alert ('more than that');
+//         } else {
+//             alert ('less than that');
+//         }
+//         answer = prompt('How many secret beaches are there in Seattle?');
+//         }
+//         alert ("that's correct!");
+//    }
+
+
+function beachLoop2(){
+
+    let answer = '';
+    do{
+        answer = prompt('How many secret beaches are there in Seattle?');
+        if(answer < 100 ){
+            alert('more than that!');
+        } else if( answer > 200){
+            alert('less than that!');
+        } else if (answer == 149){
+            alert('CORRECT! The answer is 149!');
+        } else{
+            alert('Close! The answer is 149!');
+        }
+    }while(answer != 149)
+
+    console.log("Completed");
+
 }
+
+
+function forLoopSealPup(){
+
+    let answer = prompt('How many seal pups do you want?'); 
+    let output = '';
+    for(let x = 0; x < answer; x++){
+       output += "<img class='sealpup' src='seal pup.jpeg'/>"
+    //  output = output + "<img class='star' src='star.jpg' />"
+    }
+    return document.write(output);
+}
+        
+
+// function guess2(){
+//   let number = prompt("Guess how many fingers I am holding");
+//   if(number !== 3){
+//       prompt("incorrect. try again");
+//   } else {
+//       confirm("that is correct");
+//   }
+// }
+
+
+// function backgroundColor(){
+//   let color = prompt("What's your favorite color?");
+//   return document.body.style.backgroundColor = color;
+// }
+
+// how do i use a button for the seaPup function and display the images inside the page?
+// how do i give the use a chance to cancel out of the loop?
